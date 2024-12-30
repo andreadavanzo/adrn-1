0b10edd8a933d824a693f533c22bef9360bf7396
Andrea Davanzo <info@adavanzo.com>
2024-12-30 13:43:01 +0000

    first commit

---
+++ fat-free/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '3.9.0',
+        'version' => '3.9.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'bcosca/fatfree',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bcosca/fatfree' => array(
+            'pretty_version' => '3.9.0',
+            'version' => '3.9.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'bcosca/fatfree-core' => array(
+            'pretty_version' => '3.9.0',
+            'version' => '3.9.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../bcosca/fatfree-core',
+            'aliases' => array(),
+            'reference' => '2c35ca4923d280e834222fcca473d3fe23e362d2',
+            'dev_requirement' => false,
+        ),
+    ),
+);
