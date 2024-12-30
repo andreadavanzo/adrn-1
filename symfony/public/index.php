112a0e7886423881e7bda4e75bbc4525f86313fc
Andrea Davanzo <info@adavanzo.com>
2024-12-30 16:14:42 +0000

    add phptest on symfony

--- symfony/public/index.php
+++ symfony/public/index.php
@@ -2,8 +2,44 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+$phptest_usage_start = memory_get_usage(false);
+$phptest_allocated_start = memory_get_usage(true);
+$phptest_peak_start = memory_get_peak_usage(false);
+$phptest_real_peak_start = memory_get_peak_usage(true);
+$phptest_microtime_start = microtime(true);
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+
+$kernel = new \App\Kernel('dev', true);
+
+// Create the HTTP request and pass it to the Symfony Kernel for handling
+$request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
+
+// Let the Symfony Kernel handle the request and send the response
+$response = $kernel->handle($request);
+
+$phptest_usage_end = memory_get_usage(false);
+$phptest_allocated_end = memory_get_usage(true);
+$phptest_peak_end = memory_get_peak_usage(false);
+$phptest_real_peak_end = memory_get_peak_usage(true);
+$phptest_microtime_end = microtime(true);
+
+$response->send(false);
+
+// Terminate the kernel after the response is sent
+$kernel->terminate($request, $response);
+
+echo "<pre>\n";
+echo sprintf("phptest_usage_start: %d\n", $phptest_usage_start);
+echo sprintf("phptest_allocated_start: %d\n", $phptest_allocated_start);
+echo sprintf("phptest_peak_start: %d\n", $phptest_peak_start);
+echo sprintf("phptest_real_peak_start: %d\n", $phptest_real_peak_start);
+
+echo sprintf("phptest_usage_end: %d\n", $phptest_usage_end);
+echo sprintf("phptest_allocated_end: %d\n", $phptest_allocated_end);
+echo sprintf("phptest_peak_end: %d\n", $phptest_peak_end);
+echo sprintf("phptest_real_peak_end: %d\n", $phptest_real_peak_end);
+
+echo sprintf("phptest_usage: %d\n", $phptest_usage_end - $phptest_usage_start);
+echo sprintf("phptest_allocated: %d\n", $phptest_allocated_end - $phptest_allocated_start);
+echo sprintf("phptest_microtime: %f\n", $phptest_microtime_end - $phptest_microtime_start);
--- symfony/src/Kernel.php
+++ symfony/src/Kernel.php
@@ -8,4 +8,14 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function getCacheDir(): string
+    {
+        return '/tmp/symfony/cache'; // Use a temporary directory
+    }
+
+    public function getLogDir(): string
+    {
+        return '/tmp/symfony/logs'; // Optional: Redirect logs
+    }
 }
