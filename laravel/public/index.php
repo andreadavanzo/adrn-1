b34956f694f39bb13e9770c9601fc4e91ca09203
Andrea Davanzo <info@adavanzo.com>
2024-12-30 15:05:40 +0000

    add phptest on laravel

--- laravel/public/index.php
+++ laravel/public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+$phptest_usage_start = memory_get_usage(false);
+$phptest_allocated_start = memory_get_usage(true);
+$phptest_peak_start = memory_get_peak_usage(false);
+$phptest_real_peak_start = memory_get_peak_usage(true);
+$phptest_microtime_start = microtime(true);
+
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
@@ -50,6 +56,28 @@ $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
     $request = Request::capture()
-)->send();
+)->send(false);
 
+//die('123');
 $kernel->terminate($request, $response);
+
+$phptest_usage_end = memory_get_usage(false);
+$phptest_allocated_end = memory_get_usage(true);
+$phptest_peak_end = memory_get_peak_usage(false);
+$phptest_real_peak_end = memory_get_peak_usage(true);
+$phptest_microtime_end = microtime(true);
+
+echo "<pre>\n";
+echo sprintf("phptest_usage_start: %d\n", $phptest_usage_start);
+echo sprintf("phptest_allocated_start: %d\n", $phptest_allocated_start);
+echo sprintf("phptest_peak_start: %d\n", $phptest_peak_start);
+echo sprintf("phptest_real_peak_start: %d\n", $phptest_real_peak_start);
+
+echo sprintf("phptest_usage_end: %d\n", $phptest_usage_end);
+echo sprintf("phptest_allocated_end: %d\n", $phptest_allocated_end);
+echo sprintf("phptest_peak_end: %d\n", $phptest_peak_end);
+echo sprintf("phptest_real_peak_end: %d\n", $phptest_real_peak_end);
+
+echo sprintf("phptest_usage: %d\n", $phptest_usage_end - $phptest_usage_start);
+echo sprintf("phptest_allocated: %d\n", $phptest_allocated_end - $phptest_allocated_start);
+echo sprintf("phptest_microtime: %f\n", $phptest_microtime_end - $phptest_microtime_start);
