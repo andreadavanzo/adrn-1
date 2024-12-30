91baef735c8d3bd21c584aec589e77ee9eb588b1
Andrea Davanzo <info@adavanzo.com>
2024-12-30 16:32:36 +0000

    add phptest on ci4

--- ci4/public/index.php
+++ ci4/public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+$phptest_usage_start = memory_get_usage(false);
+$phptest_allocated_start = memory_get_usage(true);
+$phptest_peak_start = memory_get_peak_usage(false);
+$phptest_real_peak_start = memory_get_peak_usage(true);
+$phptest_microtime_start = microtime(true);
+
 /*
  *---------------------------------------------------------------
  * CHECK PHP VERSION
@@ -53,4 +59,27 @@ $paths = new Config\Paths();
 // LOAD THE FRAMEWORK BOOTSTRAP FILE
 require $paths->systemDirectory . '/Boot.php';
 
-exit(CodeIgniter\Boot::bootWeb($paths));
+CodeIgniter\Boot::bootWeb($paths);
+
+
+
+$phptest_usage_end = memory_get_usage(false);
+$phptest_allocated_end = memory_get_usage(true);
+$phptest_peak_end = memory_get_peak_usage(false);
+$phptest_real_peak_end = memory_get_peak_usage(true);
+$phptest_microtime_end = microtime(true);
+
+echo "<pre>\n";
+echo sprintf("phptest_usage_start: %d\n", $phptest_usage_start);
+echo sprintf("phptest_allocated_start: %d\n", $phptest_allocated_start);
+echo sprintf("phptest_peak_start: %d\n", $phptest_peak_start);
+echo sprintf("phptest_real_peak_start: %d\n", $phptest_real_peak_start);
+
+echo sprintf("phptest_usage_end: %d\n", $phptest_usage_end);
+echo sprintf("phptest_allocated_end: %d\n", $phptest_allocated_end);
+echo sprintf("phptest_peak_end: %d\n", $phptest_peak_end);
+echo sprintf("phptest_real_peak_end: %d\n", $phptest_real_peak_end);
+
+echo sprintf("phptest_usage: %d\n", $phptest_usage_end - $phptest_usage_start);
+echo sprintf("phptest_allocated: %d\n", $phptest_allocated_end - $phptest_allocated_start);
+echo sprintf("phptest_microtime: %f\n", $phptest_microtime_end - $phptest_microtime_start);
